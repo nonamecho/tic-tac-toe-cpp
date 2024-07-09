2ccac48f6a4b93bf82c5506f9ef55397f816af25
nonamecho <joccw@hotmail.com>
2024-07-09 22:29:44 +0800

    move js files to folder

---
+++ SnakeGame/js/constants.js
@@ -0,0 +1,10 @@
+// Constants
+const OBJECT_SIZE = 10;
+const SPEED = 10;
+const HEIGHT = 600;
+const WIDTH = 600;
+
+// Utils
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
