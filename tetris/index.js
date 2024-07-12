80331a77733182097abd5f2c5d1b99108c7ff2a3
nonamecho <joccw@hotmail.com>
2024-07-12 13:30:49 +0800

    add board class, handle block in board

--- tetris/index.js
+++ tetris/index.js
@@ -1,34 +1,55 @@
 const ROW_SIZE = 30;
 const COLUMN_SIZE = 20;
-const BLOCK_SHAPES = [
-  [
-    [0, 0, 0, 0],
-    [1, 1, 1, 1],
-  ],
-  [
-    [1, 0, 0],
-    [1, 1, 1],
-  ],
-  [
-    [0, 0, 1],
-    [1, 1, 1],
-  ],
-  [
-    [1, 1],
-    [1, 1],
-  ],
-  [
-    [0, 1, 1],
-    [1, 1, 0],
-  ],
-  [
-    [0, 1, 0],
-    [1, 1, 1],
-  ],
-  [
-    [1, 1, 0],
-    [0, 1, 1],
-  ],
+const BLOCK_OBJS = [
+  {
+    shape: [
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+    ],
+    color: "#ff0000",
+  },
+  {
+    shape: [
+      [1, 0, 0],
+      [1, 1, 1],
+    ],
+    color: "#ffff00",
+  },
+  {
+    shape: [
+      [0, 0, 1],
+      [1, 1, 1],
+    ],
+    color: "#00ffff",
+  },
+  {
+    shape: [
+      [1, 1],
+      [1, 1],
+    ],
+    color: "#800080",
+  },
+  {
+    shape: [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    color: "#00ff00",
+  },
+  {
+    shape: [
+      [0, 1, 0],
+      [1, 1, 1],
+    ],
+    color: "#ff7f00",
+  },
+  {
+    shape: [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    color: "#0000ff",
+  },
 ];
 
 /**@type {HTMLCanvasElement} */
@@ -38,53 +59,66 @@ c.width = COLUMN_SIZE * 20;
 c.height = ROW_SIZE * 20;
 
 class Block {
-  constructor() {
-    this.shape = BLOCK_SHAPES[Math.floor(Math.random() * 7)];
+  constructor(game) {
+    this.game = game;
+    this.val = Math.floor((Math.random() * 70) / 10);
+    this.obj = BLOCK_OBJS[this.val];
     this.row = 0;
     this.column = Math.floor(COLUMN_SIZE / 2) - 1;
   }
   switch() {
-    this.shape = this.shape[0].map((val, index) =>
-      this.shape.map((row) => row[index]).reverse()
+    this.obj.shape = this.obj.shape[0].map((val, index) =>
+      this.obj.shape.map((row) => row[index]).reverse()
     );
+  }
+  checkIfBlock(row, column) {
+    let blocked = false;
+    for (let i = 0; i < this.obj.shape.length; i++) {
+      for (let j = 0; j < this.obj.shape[i].length; j++) {
+        if (
+          this.obj.shape[i][j] != 0 &&
+          this.game.board.val[row + i][column + j] != -1
+        ) {
+          blocked = true;
+          return true;
+        }
+      }
+    }
+    return false;
   }
   moveLeft() {
     if (this.column - 1 >= 0) {
-      this.column--;
-      return true;
-    } else {
-      return false;
+      if (!this.checkIfBlock(this.row, this.column - 1)) {
+        this.column--;
+        return true;
+      }
     }
+    return false;
   }
   moveRight() {
-    if (this.column + this.shape[0].length - 1 < COLUMN_SIZE - 1) {
-      this.column++;
-      return true;
-    } else {
-      return false;
+    if (this.column + this.obj.shape[0].length - 1 < COLUMN_SIZE - 1) {
+      if (!this.checkIfBlock(this.row, this.column + 1)) {
+        this.column++;
+        return true;
+      }
     }
-  }
-  moveUp() {
-    if (this.row - 1 > 0) {
-      this.row--;
-      return true;
-    } else {
-      return false;
-    }
+    return false;
   }
   moveDown() {
-    if (this.row + this.shape.length - 1 < ROW_SIZE - 1) {
-      this.row++;
-      return true;
-    } else {
-      return false;
+    if (this.row + this.obj.shape.length - 1 < ROW_SIZE - 1) {
+      if (!this.checkIfBlock(this.row + 1, this.column)) {
+        this.row++;
+        return true;
+      }
     }
+    this.game.add();
+    return false;
   }
   draw() {
-    ctx.fillStyle = "red";
-    for (let i = 0; i < this.shape.length; i++) {
-      for (let j = 0; j < this.shape[i].length; j++) {
-        if (this.shape[i][j]) {
+    ctx.fillStyle = this.obj.color;
+    for (let i = 0; i < this.obj.shape.length; i++) {
+      for (let j = 0; j < this.obj.shape[i].length; j++) {
+        if (this.obj.shape[i][j]) {
           ctx.fillRect(
             (this.column + j) * 20 + 1,
             (this.row + i) * 20 + 1,
@@ -97,13 +131,9 @@ class Block {
   }
 }
 
-class Game {
-  constructor() {
-    this.totalTime = 0;
-    this.lastTime = 0;
-    this.moveDowntimeToNext = 0;
-    this.moveDowntimeInterval = 500;
-    this.block = new Block();
+class Board {
+  constructor(game) {
+    this.game = game;
     const board = [];
     for (let i = 0; i < ROW_SIZE; i++) {
       const row = [];
@@ -112,9 +142,40 @@ class Game {
       }
       board.push(row);
     }
-    this.board = board;
+    this.val = board;
+  }
+  add() {
+    const block = this.game.block;
+    for (let i = 0; i < block.obj.shape.length; i++) {
+      for (let j = 0; j < block.obj.shape[i].length; j++) {
+        if (block.obj.shape[i][j]) {
+          this.val[block.row + i][block.column + j] = block.val;
+        }
+      }
+    }
+  }
+  draw() {
+    for (let i = 0; i < this.val.length; i++) {
+      for (let j = 0; j < this.val[i].length; j++) {
+        if (this.val[i][j] != -1) {
+          ctx.fillStyle = BLOCK_OBJS[this.val[i][j]].color;
+          ctx.fillRect(j * 20 + 1, i * 20 + 1, 18, 18);
+        }
+      }
+    }
+  }
+}
+
+class Game {
+  constructor() {
+    this.totalTime = 0;
+    this.lastTime = 0;
+    this.moveDowntimeToNext = 0;
+    this.moveDowntimeInterval = 500;
+    this.board = new Board(this);
+    this.block = new Block(this);
+
     addEventListener("keydown", (e) => {
-      console.info(e.key);
       switch (e.key) {
         case "ArrowLeft":
           this.block.moveLeft();
@@ -142,14 +203,17 @@ class Game {
     // Only update when meet time interval
     if (this.moveDowntimeToNext > this.moveDowntimeInterval) {
       this.moveDowntimeToNext = 0;
-      if (!this.block.moveDown()) {
-        this.block = new Block();
-      }
+      this.block.moveDown();
     }
+  }
+  add() {
+    this.board.add();
+    this.block = new Block(this);
   }
   draw() {
     ctx.reset();
     this.block.draw();
+    this.board.draw();
   }
 }
 
